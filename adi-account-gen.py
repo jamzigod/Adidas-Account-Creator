510115c9e6f7f257763464c913a04b4de3c9f059
jamzigod <jammybills@gmail.com>
2017-10-25 02:14:54 +0100

    Update adi-account-gen.py

--- adi-account-gen.py
+++ adi-account-gen.py
@@ -32,9 +32,9 @@ def create_account():
         "userVO.userAuthentication.regLogin": email,
         "userVO.userAuthentication.regPassword": password,
         "userVO.userAuthentication.confrmPassword": password,
-        "userVO.dateVO.day": "01",
-        "userVO.dateVO.month": "01",
-        "userVO.dateVO.year": "1996",
+        "userVO.dateVO.day": "01", #CHANGE
+        "userVO.dateVO.month": "01", #CHANGE
+        "userVO.dateVO.year": "1996", #CHANGE
         "agree": "true",
         "_sourcePage": "Q_zM-69uzSnv_J5IuzfaPzCh8EB353v6Dyn4g6K-ZaE=",
         "__fp": "cwbm4kzjY63tX0vBwkvCEZJLK9EzG94c0gJWx_ZSn7KmRJxEPupQ7btb9qEXch6S7V9LwcJLwhFmRiJ-SrR4x1OD6-LbZO5dzB5dlMOuFKLKYpT2bTd5fGOQ9fAgsISr"
